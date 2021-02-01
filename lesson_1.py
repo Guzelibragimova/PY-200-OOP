70b9bd826324d59bb0e45b4e960c732978b22334
gi <ibragi_guzel@mail.ru>
2021-02-01 17:45:29 +0300

    Initial commit

---
+++ lesson_1.py
@@ -0,0 +1,16 @@
+# Придумать и написать 3 абстрактных класса, описывающих любой объект.
+# Например, в качестве объектов могут выступать материальные сущности стол, дерево,
+# и даже нематериальные стек, Facebook.
+
+from typing import Any
+
+class Instagram:
+    def __init__(self, vstavka_photo: Any, poisk_po_accauntam: Any):
+        """
+        Создание и подготовка к работе объекта "Instagram"
+        :param vstavka_photo: Вставка фото в аккаунт
+        :param poisk_po_accauntam: Поиск по аккаунтам
+        """
+    
+
+
